6b1d1d023efbfdc21c60442b7585c2fd85cab331
Galangyp <galangy11@gmail.com>
2023-07-05 16:38:12 +0700

    tes deploy

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     credentials: true,
     // origin: "http://localhost:3000",
-    origin: "https://main--toko-skw.netlify.app/",
+    origin: "*",
   })
 );
 app.use(express.json({ limit: "50mb" }));
